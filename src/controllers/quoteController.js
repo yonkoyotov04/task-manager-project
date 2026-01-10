5c443c96ab9cafde4b6a78a6481d5e929ae4e78a
yonkoyotov04 <yonkoyotov04@gmail.com>
2026-01-10 11:58:15 +0200

    Updated Quote Controller

---
+++ src/controllers/quoteController.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { isAuth } from "../middlewares/authMiddleware.js";
+import quoteService from "../services/quoteService.js";
+
+const quoteController = Router();
+
+quoteController.get('/', isAuth, async (req, res) => {
+    const randomQuote = quoteService.getRandomQuote();
+
+    res.json(randomQuote ?? []);
+})
+
+quoteController.post('/', isAuth, async (req, res) => {
+    const quoteText = req.body;
+
+    await quoteService.createQuote(quoteText);
+})
