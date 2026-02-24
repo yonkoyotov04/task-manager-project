7b55010af1d325310e548d1c4fa59caf78618b83
yonkoyotov04 <yonkoyotov04@gmail.com>
2026-02-24 19:53:41 +0200

    Another attempt to fix dates

--- client/src/components/CompletedTaskCard.jsx
+++ client/src/components/CompletedTaskCard.jsx
@@ -6,7 +6,13 @@ export default function CompletedTaskCard({_id, title, completedAt, status, retu
 
     console.log(completedAt);
 
-    completedAt = new Date(completedAt).toLocaleTimeString();
+    completedAt = new Date(completedAt).toLocaleTimeString("en-GB", {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit'
+        });
 
     return (
         <div className="task-card">
